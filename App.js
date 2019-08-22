b2e55c36d22b2c183bd9ae72fe3d68adfe34b0f4
Renato Rodrigues <rtr@certi.org.br>
2019-08-22 11:18:06 -0300

    Menu

--- App.js
+++ App.js
@@ -1,114 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { createDrawerNavigator, createAppContainer } from 'react-navigation'
+import { Button, Container, Header, Left, Right, Icon, Text } from 'native-base';
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+class MyHomeScreen extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left style={{ flexDirection: 'row' }}>
+            <Icon onPress={() => this.props.navigation.openDrawer()} name="md-menu" style={{ color: 'white', marginRight: 15 }} />
+          </Left>
+          <Right>
+            <Icon name="md-cart" style={{ color: 'white' }} />
+          </Right>
+        </Header>
+        <View style={{ marginTop: 100, marginLeft: 100 }}>
+          <Button onPress={() => this.props.navigation.navigate('Notifications')} >
+            <Text>Go to notifications</Text>
+          </Button>
+        </View>
+      </Container>
+    );
+  }
+}; // End of MyHomeScreen class
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class MyNotificationsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ marginTop: 100, marginLeft: 100 }}>
+        <Button onPress={() => this.props.navigation.goBack()} >
+          <Text>Go back home</Text>
+        </Button>
+      </View>
+    );
+  }
+}; //End of MyNotificationsScreen class
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+const MyDrawerNavigator = createDrawerNavigator({
+  Home: {
+    screen: MyHomeScreen,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  Notifications: {
+    screen: MyNotificationsScreen,
   },
 });
+
+const MyApp = createAppContainer(MyDrawerNavigator);
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <MyApp />
+      </Container>
+    );
+  }
+}//End of App class
 
 export default App;
