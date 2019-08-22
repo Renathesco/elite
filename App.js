388a37da3c8aaab59a5b020d90c6beca72c209df
Renato Rodrigues <rtr@certi.org.br>
2019-08-22 11:45:41 -0300

    Fix eslint

--- App.js
+++ App.js
@@ -1,41 +1,46 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { createDrawerNavigator, createAppContainer } from 'react-navigation'
-import { Button, Container, Header, Left, Right, Icon, Text } from 'native-base';
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {createDrawerNavigator, createAppContainer} from 'react-navigation';
+import {Button, Container, Header, Left, Right, Icon, Text} from 'native-base';
 
 class MyHomeScreen extends React.Component {
   render() {
     return (
       <Container>
         <Header>
-          <Left style={{ flexDirection: 'row' }}>
-            <Icon onPress={() => this.props.navigation.openDrawer()} name="md-menu" style={{ color: 'white', marginRight: 15 }} />
+          <Left style={{flexDirection: 'row'}}>
+            <Icon
+              onPress={() => this.props.navigation.openDrawer()}
+              name="md-menu"
+              style={{color: 'white', marginRight: 15}}
+            />
           </Left>
           <Right>
-            <Icon name="md-cart" style={{ color: 'white' }} />
+            <Icon name="md-cart" style={{color: 'white'}} />
           </Right>
         </Header>
-        <View style={{ marginTop: 100, marginLeft: 100 }}>
-          <Button onPress={() => this.props.navigation.navigate('Notifications')} >
+        <View style={{marginTop: 100, marginLeft: 100}}>
+          <Button
+            onPress={() => this.props.navigation.navigate('Notifications')}>
             <Text>Go to notifications</Text>
           </Button>
         </View>
       </Container>
     );
   }
-}; // End of MyHomeScreen class
+} // End of MyHomeScreen class
 
 class MyNotificationsScreen extends React.Component {
   render() {
     return (
-      <View style={{ marginTop: 100, marginLeft: 100 }}>
-        <Button onPress={() => this.props.navigation.goBack()} >
+      <View style={{marginTop: 100, marginLeft: 100}}>
+        <Button onPress={() => this.props.navigation.goBack()}>
           <Text>Go back home</Text>
         </Button>
       </View>
     );
   }
-}; //End of MyNotificationsScreen class
+} //End of MyNotificationsScreen class
 
 const MyDrawerNavigator = createDrawerNavigator({
   Home: {
@@ -56,6 +61,6 @@ class App extends React.Component {
       </Container>
     );
   }
-}//End of App class
+} //End of App class
 
 export default App;
